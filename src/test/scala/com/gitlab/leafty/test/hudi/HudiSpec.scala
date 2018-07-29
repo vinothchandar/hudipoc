2834e988604e6d9c4f232807e4b1a251bb902d8e
SemanticBeeng <nick@semanticbeeng.com>
2018-07-29 06:22:54 -0700

    Sketched tests for consistency of ingestion and update by primary key.

--- src/test/scala/com/gitlab/leafty/test/hudi/HudiSpec.scala
+++ src/test/scala/com/gitlab/leafty/test/hudi/HudiSpec.scala
@@ -14,6 +14,8 @@ class HudiSpec extends AsyncBaseSpec with BeforeAndAfterAll {
 
   Logger.getLogger("org.apache").setLevel(Level.WARN)
 
+  // #todo use specs2 to run "sequential"ly
+
   lazy val spark: SparkSession = getSparkSession
 
   import spark.implicits._
@@ -63,6 +65,105 @@ class HudiSpec extends AsyncBaseSpec with BeforeAndAfterAll {
         .load(basePath + "/*/*/*/*")
       hoodieROViewDF1.count() shouldBe 100
     }
+
+    /**
+      * Split each csv under data_hub/ds_0001 in half (1/2) and then each group in roughly 1/3 rds
+      */
+    "ingest first 1/2 of 'acquisitions' ds_0001 in 2 commits" in {
+
+      /**
+        * ... and ingest 2 of the three 3rds
+        */
+
+      fail("Not implemented yet")
+    }
+
+    /**
+      * Split each csv under data_hub/ds_0002 in roughly 1/3 rds
+      */
+    "ingest first 1/2 of 'performances' ds_0002 in 2 commits" in {
+
+      /**
+        * ... and ingest 2 of the three 3rds
+        */
+      fail("Not implemented yet")
+    }
+
+    "check ingested data for ds_0001 is consistent" in {
+
+
+      fail("Not implemented yet")
+    }
+
+    "check ingested data for ds_0002 is consistent" in {
+
+
+      fail("Not implemented yet")
+    }
+
+    "check ingested data for ds_0001 x ds_0002 is consistent" in {
+
+      // ... where 'x' means `natural join` by `id`
+
+      fail("Not implemented yet")
+    }
+
+    "ingest second 1/2 of 'acquisitions' ds_0001 ..." in {
+
+      /**
+        * ... in three 3rds
+        */
+
+      fail("Not implemented yet")
+    }
+
+    "ingest second 1/2 of 'performances' ds_0002 ..." in {
+
+      /**
+        * ... in three 3rds
+        */
+      fail("Not implemented yet")
+    }
+
+    /**
+      * Repeat consistency checks for the second half of ingested data.
+      */
+    "check ingested data for ds_0001 is consistent" in {
+
+
+      fail("Not implemented yet")
+    }
+
+    "check ingested data for ds_0002 is consistent" in {
+
+
+      fail("Not implemented yet")
+    }
+
+    "check ingested data for ds_0001 x ds_0002 is consistent" in {
+
+      // ... where 'x' means `natural join` by `id`
+
+      fail("Not implemented yet")
+    }
+
+    /**
+      * Done checking all ingestion is consistent
+      */
+
+    /**
+      * Update records in the last "3rd" above and re-ingest
+      */
+
+
+    /**
+      * Re-apply all checks above for consistency of ingestion: essentially must show that ingesting updates does
+      * not duplicate but update records by "primary key".
+      *
+      * Note: my understanding is that this can only be achieved / observed after sync-ing with `hive`!
+      * But up to you if you think `hive` is not strictly necessary.
+      */
+
   }
 
   protected def getSparkSession: SparkSession = {
